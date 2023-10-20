d4433259870fec7e7384260dfff4247519f9c193
ErikGear <erikgalindo23@aragon.unam.mx>
2023-10-19 20:04:24 -0600

    Agrgando método de ordenamientoInsercion

--- Ejercicio1/main.js
+++ Ejercicio1/main.js
@@ -14,8 +14,38 @@ function ordenamientoBurbuja(array) {
   }
 }
 
+function ordenamientoInsercion(array){
+    let pos = undefined;
+    let tmp = undefined;
+    for (let i = 0; i < array.length - 1; i++) {
+        pos = i;
+        tmp = array[i];
+        while(pos > 0 && array[pos - 1] > tmp) {
+            array[pos] = array[pos - 1];
+            pos--;
+        }
+
+        array[pos] = tmp;
+    }
+}
+
 function busquedaSecuencial(array) {
-  for (let i = 0; i < array.length; i++) {}
+
+    //defino la longitud inicial del arreglo
+    const numeros = new Array(array.length);
+
+    // el método fill, nos permite rellenar un arreglo especificand un VALOR
+    // el primer argumento es el VALOR, el segundo parametro es desde que 
+    // posición se rrellenara el arreglo, y el TERCER parametro la posición final hasta la cual se rrelenara el arreglo con el valor indicado.
+    numeros.fill(0); //por defecto rellenaremos en su totalidad todo el array con 0's
+    
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+        if (i === array[j]) {
+            
+        }
+    }
+  }
 }
 
 function busquedaBinaria(array) {}
