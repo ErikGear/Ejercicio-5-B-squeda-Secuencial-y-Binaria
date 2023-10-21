796d5834d1f9f4712bd42ee509f41e41832abc77
ErikGear <erikgalindo23@aragon.unam.mx>
2023-10-20 20:55:33 -0600

    Dando formato al docuemnto main.js

--- Ejercicio1/main.js
+++ Ejercicio1/main.js
@@ -14,36 +14,34 @@ function ordenamientoBurbuja(array) {
   }
 }
 
-function ordenamientoInsercion(array){
-    let posicion = undefined;
-    let valorPosicion = undefined;
-    for (let i = 1; i < array.length; i++) {
-        posicion = i;
-        valorPosicion = array[i];
-        while(posicion > 0 && array[posicion - 1] > valorPosicion) {
-            array[posicion] = array[posicion - 1];
-            posicion--;
-        }
-
-        array[posicion] = valorPosicion;
+function ordenamientoInsercion(array) {
+  let posicion = undefined;
+  let valorPosicion = undefined;
+  for (let i = 1; i < array.length; i++) {
+    posicion = i;
+    valorPosicion = array[i];
+    while (posicion > 0 && array[posicion - 1] > valorPosicion) {
+      array[posicion] = array[posicion - 1];
+      posicion--;
     }
+    array[posicion] = valorPosicion;
+  }
 }
 
 function busquedaSecuencial(array) {
+  //defino la longitud inicial del arreglo
+  const numeros = new Array(array.length);
 
-    //defino la longitud inicial del arreglo
-    const numeros = new Array(array.length);
+  // el método fill, nos permite rellenar un arreglo especificand un VALOR
+  // el primer argumento es el VALOR, el segundo parametro es desde que
+  // posición se rrellenara el arreglo, y el TERCER parametro la posición final hasta la cual se rrelenara el arreglo con el valor indicado.
+  numeros.fill(0); //por defecto rellenaremos en su totalidad todo el array con 0's
 
-    // el método fill, nos permite rellenar un arreglo especificand un VALOR
-    // el primer argumento es el VALOR, el segundo parametro es desde que 
-    // posición se rrellenara el arreglo, y el TERCER parametro la posición final hasta la cual se rrelenara el arreglo con el valor indicado.
-    numeros.fill(0); //por defecto rellenaremos en su totalidad todo el array con 0's
-    
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length; j++) {
-        if (i === array[j]) {
-            
-        }
+      if (i === array[j]) {
+        numeros[i] = numeros[i] + 1;
+      }
     }
   }
 }
