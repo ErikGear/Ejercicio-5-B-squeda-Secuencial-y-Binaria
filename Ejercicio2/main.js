9477e044ac5e05763d04879f3bd61553e1d79284
ErikGear <erikgalindo23@aragon.unam.mx>
2023-10-21 22:17:31 -0600

    Agregnado solución preliminar

---
+++ Ejercicio2/main.js
@@ -0,0 +1,18 @@
+arreglo =  [8,"e",7,2,"a","d","f",2,3,1,4,3];
+
+function letrasNumeros(array) {
+    const letras = [];
+    array.forEach((item, indice) => {
+        
+        if (isNaN(item)) {
+            letras.push(item);
+            array.slice(indice,1);
+        }
+    });
+
+    return letras;
+}
+
+letras = letrasNumeros(arreglo);
+console.log(letras);
+console.log(arreglo);
