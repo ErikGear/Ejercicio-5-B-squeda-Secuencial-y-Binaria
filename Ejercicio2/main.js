3089ed6fe1681da69fbbf70ad2ffd6856bb02c56
ErikGear <erikgalindo23@aragon.unam.mx>
2023-10-21 22:21:59 -0600

    Ejericio 2 - solucionado

--- Ejercicio2/main.js
+++ Ejercicio2/main.js
@@ -1,16 +1,14 @@
-arreglo =  [8,"e",7,2,"a","d","f",2,3,1,4,3];
+arreglo = [8, "e", 7, 2, "a", "d", "f", 2, 3, 1, 4, 3];
 
 function letrasNumeros(array) {
-    const letras = [];
-    array.forEach((item, indice) => {
-        
-        if (isNaN(item)) {
-            letras.push(item);
-            array.slice(indice,1);
-        }
-    });
+  const letras = [];
+  array.forEach((item, indice) => {
+    if (isNaN(item)) {
+      letras.push(item);
+    }
+  });
 
-    return letras;
+  return letras;
 }
 
 letras = letrasNumeros(arreglo);
